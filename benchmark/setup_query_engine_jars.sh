497fefeb6841dfa149e44409e2923d26732b91f1
Flook Peter <pflooky@hotmail.com>
2024-06-04 11:29:57 +0800

    Put quotes around curl url, clone datausion-comet from github action

--- benchmark/setup_query_engine_jars.sh
+++ benchmark/setup_query_engine_jars.sh
@@ -2,19 +2,19 @@
 
 spark_major_version=$(grep sparkMajorVersion gradle.properties | cut -d= -f2)
 
-mkdir benchmark/jars
+mkdir -p benchmark/jars
 
 echo "Downloading query engine jars with Spark version: ${spark_major_version}"
 echo "Getting blaze jar..."
-curl --output benchmark/jars/blaze.jar https://github.com/kwai/blaze/releases/download/v2.0.9.1/blaze-engine-spark333-release-2.0.9.1-SNAPSHOT.jar
+curl -L -o benchmark/jars/blaze.jar "https://github.com/kwai/blaze/releases/download/v2.0.9.1/blaze-engine-spark333-release-2.0.9.1-SNAPSHOT.jar"
 echo "Finished downloading blaze jar"
 
 echo "Getting gluten jar..."
-curl --output benchmark/jars/gluten.jar https://github.com/apache/incubator-gluten/releases/download/v1.1.1/gluten-velox-bundle-spark3.4_2.12-1.1.1.jar
+curl -L -o benchmark/jars/gluten.jar "https://github.com/apache/incubator-gluten/releases/download/v1.1.1/gluten-velox-bundle-spark3.4_2.12-1.1.1.jar"
 echo "Finished downloading gluten jar"
 
 echo "Building datafusion-comet..."
-git clone git@github.com:apache/datafusion-comet.git benchmark/build
+#git clone git@github.com:apache/datafusion-comet.git benchmark/build
 cd benchmark/build/datafusion-comet
 make release PROFILES="-Pspark-${spark_major_version}"
 ls spark/target/comet-spark-*-SNAPSHOT.jar
