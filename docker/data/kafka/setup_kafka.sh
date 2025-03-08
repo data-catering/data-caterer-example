4203e20a7bbe8cb77fce8db5481b50aabe7da24f
Flook Peter <pflooky@hotmail.com>
2025-03-08 12:19:41 +0800

    Update to 0.15.3, add in kafka relationships example

--- docker/data/kafka/setup_kafka.sh
+++ docker/data/kafka/setup_kafka.sh
@@ -9,3 +9,6 @@ echo 'Successfully created the following topics:'
 kafka-topics --bootstrap-server $host:29092 --list
 
 #kafka-topics --delete --topic account-topic --bootstrap-server localhost:9092
+#kafka-topics --bootstrap-server localhost:9092 --create --topic customer-product-topic --replication-factor 1 --partitions 1
+#kafka-console-consumer --bootstrap-server localhost:9092 --topic customer-product-topic --from-beginning
+#kafka-console-consumer --bootstrap-server localhost:9092 --topic customer-topic --from-beginning
